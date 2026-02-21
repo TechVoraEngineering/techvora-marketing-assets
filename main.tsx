28d63b5257f363fd3d09e8983b3538c7ee49eb0d
Josh <josh@Joshs-Laptop.local>
2026-02-21 09:50:33 -0600

    Fixing folder structure

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./readiness-assessment";
+import App from "./readiness-assessment/readiness-assessment";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
